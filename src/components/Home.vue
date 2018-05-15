e4beff956436d496be69f85f23b93436b3a6eae0
liuhaoran <lhr@lhrsite.com>
2018-05-15 15:27:03 +0800

    首页

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="title">
+        这是首页
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+.title{
+    text-align: center;
+}
+</style>
